89384292d8b51d7f8bd02627ff81c7b93d19f5cd
Justin Daining <justin@eventboard.io>
2016-03-07 16:53:23 -0700

    styles yeaaaa

--- app/cities/slc/index.php
+++ app/cities/slc/index.php
@@ -13,10 +13,14 @@ $status = is_odd($day);
 
 if ( $status == 'even' ) {
 	$mcstatus = 'open';
+	$mcclass = 'alert-success';
 	$ccstatus = 'closed';
+	$ccclass = 'alert-danger';
 } else {
 	$mcstatus = 'closed';
+	$mcclass = 'alert-danger';
 	$ccstatus = 'open';
+	$ccclass = 'alert-success';
 }
 
 ?>
@@ -51,45 +55,48 @@ if ( $status == 'even' ) {
   	<link rel="stylesheet" href="../css/main.css">
 
 </head>
-<body>
+<body class="text-center">
 
-	<div class="header center">
-		<div class="inner">
-			<?php if ( $status == 'odd' ) : ?>
-			<h1>Oddly enough...</h1>
-			<?php else : ?>
-			<h1>I can't even...</h1>
-			<?php endif; ?>
-			<h3>Where can I ride my bike on <?php echo date('l, F j, Y'); ?>?</h3>
-		</div>
-	</div>
-	
-	<div class="millcreek <?php echo $mcstatus; ?> center">
-		<div class="inner">
-			<h2>Upper MillCreek</h2>
-			<h3>is <?php echo $mcstatus; ?> to bicycles today.*</h3>
-		</div>
-	</div>
-	
-	<div class="city_creek <?php echo $ccstatus; ?> center">
-		<div class="inner">
-			<h2>City Creek Canyon</h2>
-			<h3>is <?php echo $ccstatus; ?> to bicycles today.**</h3>
-		</div>
-	</div>
+<div class="container-fluid">
+  <div class="row">
+  	<div class="col-md-12">
+			  <div class="page-header">
+			    <?php if ( $status == 'odd' ) : ?>
+					<h1>Oddly enough...</h1>
+					<?php else : ?>
+					<h1>I can't even...</h1>
+					<?php endif; ?>
+					<h3>Where can I ride my bike on <?php echo date('l, F j, Y'); ?>?</h3>
+			  </div>
+			  <div class="panel-body">
+			    <div class="alert <?php echo $mcclass; ?>">
+						<h2>Upper MillCreek</h2>
+						<h3>is <?php echo $mcstatus; ?> to bicycles today.*</h3>
+					</div>
+					<div class="alert <?php echo $ccclass; ?>">
+						<h2>City Creek Canyon</h2>
+						<h3>is <?php echo $ccstatus; ?> to bicycles today.**</h3>
+					</div>
+					<div class="fine_print">
+						<p class="text-muted">* Upper MillCreek Trails (Big Water, Little Water, Great Western) are open to cyclists on even days. <a target="_blank" href="http://slco.org/recreation/parks/millCreekCanyon/">Click here for more information.</a></p>
+						<p class="text-muted">** City Creek is open to bicycles all winter and on odd days from the last Monday in May until the last even day in September. Bicycles are not allowed on Memorial Day, July 4, Pioneer Day, or Labor Day, regardless of the day. <a target="_blank" href="http://www.bikeslc.com/wheretoride/multi-usepavedtrails/citycreekmemorygrove.html">Click here for more information.</a></p>
+					</div>
+			</div>
+  	</div>
+  </div>
+</div>
 
-	<div class="fine_print">
-		<div class="inner">
-			<p>* Upper MillCreek Trails (Big Water, Little Water, Great Western) are open to cyclists on even days. <a target="_blank" href="http://slco.org/recreation/parks/millCreekCanyon/">Click here for more information.</a></p>
-			<p>** City Creek is open to bicycles all winter and on odd days from the last Monday in May until the last even day in September. Bicycles are not allowed on Memorial Day, July 4, Pioneer Day, or Labor Day, regardless of the day. <a target="_blank" href="http://www.bikeslc.com/wheretoride/multi-usepavedtrails/citycreekmemorygrove.html">Click here for more information.</a></p>
+<div class="container-fluid">
+  <div class="row">
+  	<div class="col-md-12">
+			<div class="footer center">
+				<div class="inner">
+					<p>&copy; <?php echo date('Y'); ?> <a target="_blank" href="http://www.justindaining.com">JustinDaining.com</a></p>
+				</div>
+			</div>
 		</div>
 	</div>
-
-	<div class="footer center">
-		<div class="inner">
-			<p>&copy; <?php echo date('Y'); ?> <a target="_blank" href="http://www.justindaining.com">JustinDaining.com</a></p>
-		</div>
-	</div>
+</div>
 		
 	<!-- JS 
 	================================================== -->
